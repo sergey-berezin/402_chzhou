d0c72619cd3d1b7aa1330b28fdf9b8c3637e57a7
lynneZ-KNP <zhous123456@yahoo.com>
2023-12-16 14:08:39 +0300

    lab_3

--- Lab2/BertViewModel/BaseViewModel.cs
+++ Lab2/BertViewModel/BaseViewModel.cs
@@ -15,4 +15,21 @@ namespace BertViewModel
         protected void RaisePropertyChanged([CallerMemberName] string propertyName = "") =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+    //public class MainViewModel : INotifyPropertyChanged
+    //{
+    //    private readonly PersistenceService _persistenceService;
+    //    private AppState _appState;
+
+    //    public MainViewModel()
+    //    {
+    //        _persistenceService = new PersistenceService();
+    //        _appState = _persistenceService.LoadState();
+
+    //        // Initialize other ViewModel properties using _appState
+    //    }
+
+    //    // Add methods to handle questions and update texts
+    //    // Make sure to call _persistenceService.SaveState(_appState) when changes are made
+    //}
+
 }
